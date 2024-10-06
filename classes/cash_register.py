2098f1ab6cb4516c71bc17b5f160a58a4ef97095
giulia-colombo <giulia.colombo.127@gmail.com>
2024-10-06 19:26:21 +0200

    chore: Initial commit

---
+++ classes/cash_register.py
@@ -0,0 +1,13 @@
+class CashRegister:
+    def __init__(self):
+        self.current_order = []
+    
+    # def display_products: displays list of products, as a reminder
+
+    # def add_product: adds products to the current order
+
+    # def apply_discounts: applies appropriate discounts to order
+
+    # def calculate_total: sums value of products in the current order
+
+
