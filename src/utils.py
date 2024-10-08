9b520f17bff93be2e3ed359c745f6d5002db8158
giulia-colombo <giulia.colombo.127@gmail.com>
2024-10-08 18:23:09 +0200

    feat: Add sanitize_user_input to utils.
    test: Add test for sanitize_user_input

--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,5 @@
-# TO DO
-# def sanitize_user_input
+def sanitize_user_input(input):
+    '''Takes user input and returns it as a lowercase string, without trailing spaces and a single space in between every word'''
+    sanitized_input = input.lower().strip()
+    sanitized_input = " ".join(sanitized_input.split())
+    return sanitized_input
